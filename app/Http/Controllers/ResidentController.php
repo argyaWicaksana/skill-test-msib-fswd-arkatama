5a3b033d4d22dabf0daff9ed50085cbda55cb33b
argyaWicaksana <argya7@gmail.com>
2024-01-02 12:47:06 +0700

    add data validation

--- app/Http/Controllers/ResidentController.php
+++ app/Http/Controllers/ResidentController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ResidentController extends Controller
 {
     public function store(Request $request)
     {
+        $rules = [
+            'data' => 'required|string',
+        ];
+        
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $messages = $validator->messages();
+            $errors = $messages->all();
+            return response()->json([
+                'errors' => $errors
+            ]);
+        }
+
         $data = $request->input('data');
         $data = strtoupper($data);
         $splitData = preg_split('/(\d+)/', $data, -1, PREG_SPLIT_DELIM_CAPTURE);;
@@ -22,6 +36,7 @@ class ResidentController extends Controller
         $age = $splitData[1];
         $city = str_replace($deleteWord, '', $splitData[2]);
         $city = trim($city);
+        // dd($name, $age, $city);
 
         $createdAt = $updatedAt = now();
 

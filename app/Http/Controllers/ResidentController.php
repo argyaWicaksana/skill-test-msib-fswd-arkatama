c65b87d09c080b7e64b45dd9cb86eed6e0f2edca
argyaWicaksana <argya7@gmail.com>
2024-01-02 11:54:15 +0700

    Study Case Arkatama

---
+++ app/Http/Controllers/ResidentController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ResidentController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = $request->input('data');
+        $data = strtoupper($data);
+        $splitData = preg_split('/(\d+)/', $data, -1, PREG_SPLIT_DELIM_CAPTURE);;
+        $deleteWord = [
+            'TAHUN',
+            'TH',
+            'THN',
+        ];
+
+        $name = rtrim($splitData[0]);
+        $age = $splitData[1];
+        $city = str_replace($deleteWord, '', $splitData[2]);
+        $city = trim($city);
+
+        $createdAt = $updatedAt = now();
+
+        DB::table('residents')->insert([
+            'name' => $name,
+            'age' => $age,
+            'city' => $city,
+            "created_at" => $createdAt,
+            "updated_at" => $updatedAt,
+        ]);
+
+        return response()->json([
+            'name' => $name,
+            'age' => $age,
+            'city' => $city,
+            "created_at" => $createdAt,
+            "updated_at" => $updatedAt,
+        ]);
+    }
+}
